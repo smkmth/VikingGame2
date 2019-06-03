1216a0364a94f14d830995fd912d275a2ba91091
smkmth <dandavidnoon@gmail.com>
2019-06-03 15:32:26 +0100

    updated stuff

--- Assets/Scripts/Crafting/CraftingMenu.cs
+++ Assets/Scripts/Crafting/CraftingMenu.cs
@@ -10,9 +10,12 @@ public class CraftingMenu : MonoBehaviour {
     public GameObject craftingMenu;
     public GameObject craftingSlotPrefab;
     public GameObject recipesPanel;
-    public GameObject ingredientsPanel;
+    public GameObject missingIngredientsPanel;
+    public GameObject foundIngredientsPanel;
     public GameObject ingredientsPrefab;
     public Inventory playerInventory;
+
+    private CraftingRecipe selectedItem;
 
     // Use this for initialization
     void Start () {
@@ -35,46 +38,110 @@ public class CraftingMenu : MonoBehaviour {
 		
 	}
 
-    public List<Item> CheckToBuild(CraftingRecipe recipeToCheck)
+    public List<Item> CheckItemsOwned(CraftingRecipe recipeToCheck)
+    {
+
+        List<Item> itemsHave = new List<Item>();
+        for (int i = 0; i < recipeToCheck.requiredIngredients.Count; i++)
+        {
+            if (playerInventory.GetItemCount(recipeToCheck.requiredIngredients[i]) != 0)
+            {
+                itemsHave.Add(recipeToCheck.requiredIngredients[i]);
+
+            }
+        }
+        return itemsHave;
+    }
+
+    public List<Item> CheckItemsMissing(CraftingRecipe recipeToCheck)
     {
         
-        List<Item> itemMissing = new List<Item>();
+        List<Item> itemsMissing = new List<Item>();
         for (int i = 0; i < recipeToCheck.requiredIngredients.Count; i++)
         {
             if (playerInventory.GetItemCount(recipeToCheck.requiredIngredients[i]) == 0)
             {
-                itemMissing.Add(recipeToCheck.requiredIngredients[i]);
+                itemsMissing.Add(recipeToCheck.requiredIngredients[i]);
 
             }
         }
-        return itemMissing;
+        return itemsMissing;
     }
 	public void ToggleCraftingMenu(bool isCrafting)
     {
 
         craftingMenu.SetActive(isCrafting);
-        UpdateCraftingMenu();
-
-
-
+        RemoveChildren();
     }
-    public void UpdateCraftingMenu()
+    public void UpdateCraftingMenu(CraftingRecipe selectedRecipe)
     {
-        List<Item> missingItems = CheckToBuild(masterCraftingRecipes[0]);
+        List<Item> missingItems = CheckItemsMissing(selectedRecipe);
+        List<Item> ownedItems = CheckItemsOwned(selectedRecipe);
         foreach (Item missingItem in missingItems)
         {
-            GameObject missingItemSlot =Instantiate(ingredientsPrefab, ingredientsPanel.transform);
+            GameObject missingItemSlot =Instantiate(ingredientsPrefab, missingIngredientsPanel.transform);
             Image missingItemImage = missingItemSlot.GetComponent<Image>();
             missingItemImage.sprite = missingItem.icon;
         }
 
+        foreach (Item ownedItem in ownedItems)
+        {
+            GameObject ownedItemSlot = Instantiate(ingredientsPrefab, foundIngredientsPanel.transform);
+            Image ownedItemImage = ownedItemSlot.GetComponent<Image>();
+            ownedItemImage.sprite = ownedItem.icon;
+        }
 
-        
+
+
+    }
+
+    public int RecipeToIndex(CraftingRecipe recipeToFind)
+    {
+        int index = -1;
+        for(int i = 0; i < masterCraftingRecipes.Count; i++)
+        {
+            if (masterCraftingRecipes[i].name == recipeToFind.name)
+            {
+                index = i;
+            }
+
+        }
+        return index;
     }
    
 
     public void OnClickCraftSlot(CraftingRecipe selectedCraftingRecipe)
     {
+        RemoveChildren();
+        selectedItem = selectedCraftingRecipe;
+        UpdateCraftingMenu(selectedCraftingRecipe);
 
+    }
+    public void CraftItem()
+   {
+        if (CheckItemsMissing(selectedItem).Count == 0)
+        {
+            Debug.Log("HERE");
+            playerInventory.AddItem(selectedItem.itemProduced);
+        }
+    }
+
+    void RemoveChildren()
+    {
+        selectedItem = null;
+        {
+            int childCount = missingIngredientsPanel.transform.childCount;
+            for (int i = childCount - 1; i >= 0; --i)
+            {
+                GameObject.Destroy(missingIngredientsPanel.transform.GetChild(i).gameObject);
+            }
+        }
+        {
+            int childCount = foundIngredientsPanel.transform.childCount;
+            for (int i = childCount - 1; i >= 0; --i)
+            {
+                GameObject.Destroy(foundIngredientsPanel.transform.GetChild(i).gameObject);
+            }
+        }
     }
 }
